8c1d8bad8c63f3bd42cdf668b36e30b8bda80af4
Nourbenmed <benmohamednour30@gmail.com>
2025-02-27 18:25:19 +0100

    fiest commit

---
+++ src/test/java/com/e2eTests/automatedTests/authentication/pageObjects/AuthenticationPage.java
@@ -0,0 +1,53 @@
+package com.e2eTests.automatedTests.authentication.pageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.e2eTests.automatedTests.utilis.BasePage;
+
+public class AuthenticationPage extends BasePage {
+
+	private static final String HOME_PAGE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+	public static String object;
+
+	public AuthenticationPage() {
+		PageFactory.initElements(driver, this);
+	}
+	public void goToHomePage(){
+		driver.get(HOME_PAGE_URL);
+		wait.forLoading(5);
+	}
+
+	/*Locators*/
+	final static String USER_NAME = "username";
+	final static String USER_PASSWORD = "password";
+	final static String BUTTON_LOGIN_ID = "//button[@type='submit']";
+	final static String USER_ADMIN ="//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/div/p";
+
+	@FindBy(how = How.NAME, using = USER_NAME)
+	public static WebElement userName;
+	@FindBy(how = How.NAME, using = USER_PASSWORD)
+	public static WebElement userPassword;
+	@FindBy(how = How.XPATH, using = BUTTON_LOGIN_ID)
+	public static WebElement btnLogin;
+	@FindBy(how = How.XPATH, using = USER_ADMIN)
+	public static WebElement userAdmin;
+
+
+	/*Methods*/
+	public void FillUserName(String name) {
+		userName.clear();
+		userName.sendKeys(name);
+	}
+
+	public void FillPassword(String password) {
+		userPassword.clear();
+		userPassword.sendKeys(password);
+	}
+
+	public void ClickBtnLogin() {
+		btnLogin.click();
+	}
+}
---
+++ src/test/java/com/e2eTests/automatedTests/authentication/stepDefinitions/AuthenticationStepDefinitions.java
@@ -0,0 +1,50 @@
+package com.e2eTests.automatedTests.authentication.stepDefinitions;
+
+import com.e2eTests.automatedTests.utilis.Setup;
+import cucumber.api.java.en.Given;
+
+import com.e2eTests.automatedTests.authentication.pageObjects.AuthenticationPage;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class AuthenticationStepDefinitions {
+	public WebDriver driver;
+	private AuthenticationPage authenticationPage = new AuthenticationPage();
+	public void AuthenticationStepDefinitions() {
+		driver = Setup.driver;
+		PageFactory.initElements(driver, AuthenticationPage.class);
+	}
+
+	@Given("^Je me connecte à l'application OrangeHRM$")
+	public void jeMeConnecteÀLApplicationOrangeHRM() throws InterruptedException {
+			authenticationPage.goToHomePage();
+			Thread.sleep(5000);
+	}
+
+
+	@When("^Je saisie Username \"([^\"]*)\"$")
+	public void jeSaisieUsername(String name)  {
+		authenticationPage.FillUserName(name);
+	}
+
+	@When("^Je saisie Password \"([^\"]*)\"$")
+	public void jeSaisiePassword(String password) {
+		authenticationPage.FillPassword(password);
+	}
+
+	@When("^Je clique sur le boutton login$")
+	public void jeCliqueSurLeBouttonLogin() throws InterruptedException {
+			authenticationPage.ClickBtnLogin();
+	}
+
+	@Then("^Je me redirige vers le compte admin$")
+	public void jeMeRedirigeVersLeCompteAdmin() throws InterruptedException {
+			Thread.sleep(5000);
+			String textMessage = AuthenticationPage.userAdmin.getText();
+			Assert.assertTrue(textMessage.contains("Time at Work"));
+		}
+}
